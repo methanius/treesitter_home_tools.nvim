55bc175e2c21a0d30048ed7f65f22d01317c8c6b
ClausNMadsen <claus.normann@hotmail.com>
2024-02-15 21:33:10 +0100

    Refactor +typing +include cword

--- lua/treesitter_home_tools/init.lua
+++ lua/treesitter_home_tools/init.lua
@@ -1,8 +1,9 @@
 ---@class (exact) TreesitterHomeTools.Commands
 ---@field options TreesitterHomeTools.Config
----@field toggle_next_bool function?
----@field toggle_previous_bool function?
+---@field toggle_next_bool fun(boolean?)?
+---@field toggle_previous_bool fun(boolean?)?
 ---@field setup function
+---@field dot_repeater? fun(fun): fun()
 local M = {}
 
 ---@class (exact) TreesitterHomeTools.Config
@@ -18,6 +19,9 @@ function M.setup(opts)
   M.options = vim.tbl_deep_extend("force", {}, defaults, opts or {})
   if M.options.enable_toggle_boolean then
     local toggle_bool = require("treesitter_home_tools.toggle_bool")
+    vim.api.nvim_create_user_command("ToggleNextBool", function()
+      toggle_bool.toggle_next_bool()
+    end, { bar = true, bang = true, nargs = "?", desc = "Toggles next boolean using Treesitter" })
     M.toggle_next_bool = toggle_bool.toggle_next_bool
     M.toggle_previous_bool = toggle_bool.toggle_previous_bool
   end
--- lua/treesitter_home_tools/toggle_bool.lua
+++ lua/treesitter_home_tools/toggle_bool.lua
@@ -1,7 +1,24 @@
+---@class NodePosition
+---@field row integer
+---@field col integer
+
+---@alias TreesitterQuery string
+---
+---@class NodeLocatorOpts
+---@field query TreesitterQuery
+---@field include_current boolean
+
 ---@class ToggleBool
+---@field toggle_next_bool? fun(boolean)
+---@field repeatable_toggle_next_bool? fun(boolean)
+---@field toggle_previous_bool? fun(boolean)
+---@field _repeated boolean
+
+---@type ToggleBool
 local M = {}
 
-local toggle_bool_under_cursor = function()
+---Toggles bool under cursor by going into insert mode
+local swap_bool_under_cursor = function()
   local word = vim.fn.expand("<cword>")
   if word == "true" then
     vim.cmd("normal! m`ciwfalse")
@@ -14,51 +31,100 @@ local toggle_bool_under_cursor = function()
   end
 end
 
---- Jumps to next boolean and switches its value. If no boolean is found, it does nothing
-function M.toggle_next_bool()
+---Find the next node result from query input.
+---If standing on a node result, get the last position of it
+---@param opts NodeLocatorOpts
+---@return NodePosition?
+---@package
+local get_next_queried_node_position = function(opts)
   local cursor_line, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
   local filetype = vim.bo[vim.api.nvim_get_current_buf()].filetype
   local scope = vim.treesitter.get_node():tree():root()
-  local ok, s_query = pcall(vim.treesitter.query.parse, filetype, "([(true) (false)]) @bools")
+  local ok, s_query = pcall(vim.treesitter.query.parse, filetype, opts.query)
   if ok == true then
     for _, node, _ in
       s_query:iter_captures(scope, vim.api.nvim_get_current_buf(), cursor_line - 1, -1)
     do
-      local node_end_line, node_end_col, _ = node:end_()
-      if
-        node_end_line + 1 > cursor_line
-        or (node_end_line + 1 == cursor_line and node_end_col > cursor_col)
-      then
-        vim.api.nvim_win_set_cursor(0, { node_end_line + 1, node_end_col - 1 })
-        toggle_bool_under_cursor()
-        break
+      local _, start_col, end_row, end_col = node:range()
+      if opts.include_current then
+        if end_row + 1 > cursor_line or (end_row + 1 == cursor_line and end_col > cursor_col) then
+          return { end_row + 1, end_col - 1 }
+        end
+      else
+        if end_row + 1 > cursor_line or (end_row + 1 == cursor_line and start_col > cursor_col) then
+          return { end_row + 1, end_col - 1 }
+        end
       end
     end
   end
 end
 
---- Jumps to previous boolean and switches its value. If no boolean is found, it does nothing
-function M.toggle_previous_bool()
+---Find the previous node result from query input.
+---If standing on a node result, get the last position of it
+---@param opts NodeLocatorOpts
+---@return NodePosition?
+---@package
+local get_previous_queried_node_position = function(opts)
   local cursor_line, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
   local filetype = vim.bo[vim.api.nvim_get_current_buf()].filetype
   local scope = vim.treesitter.get_node():tree():root()
-  local ok, s_query = pcall(vim.treesitter.query.parse, filetype, "([(true) (false)]) @bools")
+  local ok, s_query = pcall(vim.treesitter.query.parse, filetype, opts.query)
   if ok == true then
     local res_pos = nil
     for _, node, _ in s_query:iter_captures(scope, vim.api.nvim_get_current_buf(), 0, cursor_line) do
-      local node_start_line, node_start_col, _ = node:start()
-      local node_end_line, node_end_col, _ = node:end_()
-      if
-        node_start_line + 1 < cursor_line
-        or (node_start_line + 1 == cursor_line and node_start_col < cursor_col)
-      then
-        res_pos = { node_end_line + 1, node_end_col - 1 }
+      local start_row, start_col, end_row, end_col = node:range()
+      if opts.include_current then
+        if
+          start_row + 1 < cursor_line
+          or (start_row + 1 == cursor_line and start_col < cursor_col)
+        then
+          res_pos = { end_row + 1, end_col - 1 }
+        end
+      else
+        if
+          start_row + 1 < cursor_line
+          or (start_row + 1 == cursor_line and end_col < cursor_col )
+        then
+          res_pos = { end_row + 1, end_col - 1 }
+        end
       end
     end
-    if res_pos ~= nil then
-      vim.api.nvim_win_set_cursor(0, res_pos)
-      toggle_bool_under_cursor()
-    end
+    return res_pos
+  end
+end
+
+---@type TreesitterQuery
+local boolean_query = "([(true) (false)])@bools"
+
+--- Jumps to next boolean and switches its value. If no boolean is found, it does nothing
+--- @param include_current_word? boolean
+function M.toggle_next_bool(include_current_word)
+  if include_current_word == nil then
+    include_current_word = true
+  end
+  local pos = get_next_queried_node_position({
+    query = boolean_query,
+    include_current = include_current_word,
+  })
+  if pos ~= nil then
+    vim.api.nvim_win_set_cursor(0, { pos[1], pos[2] })
+    swap_bool_under_cursor()
+  end
+end
+
+--- Jumps to previous boolean and switches its value. If no boolean is found, it does nothing
+--- @param include_current_word? boolean
+function M.toggle_previous_bool(include_current_word)
+  if include_current_word == nil then
+    include_current_word = true
+  end
+  local res_pos = get_previous_queried_node_position({
+    query = boolean_query,
+    include_current = include_current_word,
+  })
+  if res_pos ~= nil then
+    vim.api.nvim_win_set_cursor(0, res_pos)
+    swap_bool_under_cursor()
   end
 end
 
