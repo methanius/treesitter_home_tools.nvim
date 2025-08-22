a9a4c5eee8cb5ce9e9d794c6f9d0c457e6acd965
Claus Normann Wibrand <claus.normann@hotmail.com>
2025-08-22 20:52:17 +0200

    expand python test file

--- test.py
+++ test.py
@@ -6,3 +6,20 @@ True
 False
 
 {x for x in range(1)}
+
+[]
+
+h = { 10, 101, 1230, 12491924, "star", 1203 }
+
+[10, 1230, 1240, 124125, 1249, "gasg", 1.1]
+
+def f(a, b, c):
+    "whhat"
+
+def g(a):
+    "what"
+
+def z(a, b):
+    "what"
+
+z(10, f(20,23,1))
